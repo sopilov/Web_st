4e89330ea3c207323646e0829f4776e83d56a05d
sopilov <43342760+sopilov@users.noreply.github.com>
2018-09-28 16:52:39 +0300

    Add files via upload

---
+++ L_4/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+    }
+}
